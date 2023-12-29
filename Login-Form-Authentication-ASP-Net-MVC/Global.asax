6b01cc3d471c351d35879333940ebe041e06706a
rsaransh72 <rsaransh72@gmail.com>
2023-12-29 16:53:46 +0530

    Add project files.

---
+++ Login-Form-Authentication-ASP-Net-MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Login_Form_Authentication_ASP_Net_MVC.MvcApplication" Language="C#" %>
---
+++ Login-Form-Authentication-ASP-Net-MVC/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Login_Form_Authentication_ASP_Net_MVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
